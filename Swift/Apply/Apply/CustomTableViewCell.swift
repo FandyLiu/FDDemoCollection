f003bfbab2d15b3947af2ccded9d3abd682d11f1
QianTuFD <liuhuan@QianTuFDdeiMac.local>
2017-03-29 18:17:23 +0800

    cell

--- Swift/Apply/Apply/CustomTableViewCell.swift
+++ Swift/Apply/Apply/CustomTableViewCell.swift
@@ -10,9 +10,8 @@ import UIKit
 
 enum CustomTableViewCellType {
     case describe(title: String, subtitle: String)
-    case input(title: String)
-    case inputWithPlaceholder(title: String, placeholder: String)
-    case inputWithRightPlaceholder(title: String, rightplaceholder: String)
+    case input0(title: String, placeholder: String)
+    case input1(title: String, rightplaceholder: String)
 }
 
 
@@ -33,20 +32,28 @@ class CustomTableViewCell: UITableViewCell {
         descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
         descriptionLabel.backgroundColor = UIColor.red
         descriptionLabel.text = "aaaa"
-        descriptionLabel.textColor = 
-        descriptionLabel.font = UIFont.systemFont(ofSize: 28.0)
+        descriptionLabel.textColor = COLOR_666666
+        descriptionLabel.font = FONT_28PX
         return descriptionLabel
     }()
     
     let titleLabel: UILabel = {
         let titleLabel = UILabel()
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.backgroundColor = UIColor.green
+        titleLabel.text = "你好你好："
+        titleLabel.textColor = COLOR_222222
+        titleLabel.font = FONT_28PX
         return titleLabel
     }()
     
     let textField: UITextField = {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = UIColor.blue
+        textField.text = "你好你好："
+        textField.textColor = COLOR_222222
+        textField.font = FONT_28PX
         return textField
     }()
     
@@ -58,6 +65,7 @@ class CustomTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
         setupUI()
     }
     
@@ -66,21 +74,54 @@ class CustomTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setupUI() {
+    fileprivate func setupUI() {
         contentView.addSubview(mycontentView)
         mycontentView.addSubview(descriptionLabel)
         mycontentView.addSubview(titleLabel)
+        mycontentView.addSubview(textField)
+        mycontentView.addSubview(arrowImageView)
         
         // mycontentView
         ({
-            contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-50-[mycontentView]-50-|", options: NSLayoutFormatOptions.alignAllCenterY, metrics: nil, views: ["mycontentView" : mycontentView]))
+            contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-25-[mycontentView]-25-|", options: NSLayoutFormatOptions.alignAllCenterY, metrics: nil, views: ["mycontentView" : mycontentView]))
             contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[mycontentView]-0-|", options: NSLayoutFormatOptions.alignAllCenterX, metrics: nil, views: ["mycontentView" : mycontentView]))
             }())
-        
         // descriptionLabel
         ({
-            mycontentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[descriptionLabel]-0-|", options: NSLayoutFormatOptions.alignAllCenterY, metrics: nil, views: ["descriptionLabel" : descriptionLabel]))
-            mycontentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[descriptionLabel]-|", options: NSLayoutFormatOptions.alignAllCenterX, metrics: nil, views: ["descriptionLabel" : descriptionLabel]))
+            let leftConstraint = NSLayoutConstraint(item: descriptionLabel, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: mycontentView, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: 0.0)
+            let rightConstraint = NSLayoutConstraint(item: descriptionLabel, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: mycontentView, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: 0.0)
+            let centerYConstraint = NSLayoutConstraint(item: descriptionLabel, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: mycontentView, attribute: NSLayoutAttribute.centerY, multiplier: 1.0, constant: 0.0)
+            mycontentView.addConstraints([leftConstraint, rightConstraint, centerYConstraint])
+            }())
+        
+        
+        // titleLabel
+        ({
+            let text: NSString = "你好你好："
+            let contentSize = CGSize(width: CGFloat(MAXFLOAT) , height: CGFloat(MAXFLOAT))
+            let width = text.textSizeWith(contentSize: contentSize, font: titleLabel.font).width + 2
+            
+            let leftConstraint = NSLayoutConstraint(item: titleLabel, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: mycontentView, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: 0.0)
+            let centerYConstraint = NSLayoutConstraint(item: titleLabel, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: mycontentView, attribute: NSLayoutAttribute.centerY, multiplier: 1.0, constant: 0.0)
+            let widthConstraint = NSLayoutConstraint(item: titleLabel, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1.0, constant: width)
+            mycontentView.addConstraints([leftConstraint, centerYConstraint])
+            titleLabel.addConstraint(widthConstraint)
+            }())
+        
+        // textField
+        
+        ({
+            let leftConstraint = NSLayoutConstraint(item: textField, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: titleLabel, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: 2.0)
+            let rightConstraint = NSLayoutConstraint(item: textField, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: mycontentView, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: -25.0)
+            let centerYConstraint = NSLayoutConstraint(item: textField, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: mycontentView, attribute: NSLayoutAttribute.centerY, multiplier: 1.0, constant: 0.0)
+            mycontentView.addConstraints([leftConstraint, rightConstraint, centerYConstraint])
+            }())
+        // arrowImageView
+        
+        ({
+            let rightConstraint = NSLayoutConstraint(item: arrowImageView, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: mycontentView, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: -2.0)
+            let centerYConstraint = NSLayoutConstraint(item: arrowImageView , attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: mycontentView, attribute: NSLayoutAttribute.centerY, multiplier: 1.0, constant: 0.0)
+            mycontentView.addConstraints([rightConstraint, centerYConstraint])
             }())
         
     }
--- Swift/Apply/Apply/FDUtils.swift
+++ Swift/Apply/Apply/FDUtils.swift
@@ -8,8 +8,39 @@
 
 import UIKit
 
-func colorWithHexString(str: String) -> UIColor {
-    return UIColor(colorLiteralRed: <#T##Float#>, green: <#T##Float#>, blue: <#T##Float#>, alpha: <#T##Float#>)
+extension Int {
+    var f: CGFloat {
+        return CGFloat(self)
+    }
 }
+
+extension UIColor {
+    class func rgbColorWith(hexValue: Int) -> UIColor {
+        return UIColor(red: ((hexValue & 0xFF0000) >> 16).f / 255.0,
+                       green: ((hexValue & 0xFF00) >> 8).f / 255.0,
+                       blue: (hexValue & 0xFF).f / 255.0,
+                       alpha: 1.0)
+    }
+}
+
+extension UIFont {
+    class func fontWith(pixel: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: pixel / 96.f * 72.f)
+    }
+}
+
+extension NSString {
+    func textSizeWith(contentSize:CGSize, font: UIFont) -> CGSize {
+        let attrs = [NSFontAttributeName: font]
+        return self.boundingRect(with: contentSize, options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: attrs, context: nil).size
+    }
+}
+
+
+let COLOR_666666 = UIColor.rgbColorWith(hexValue: 0x666666)
+let COLOR_222222 = UIColor.rgbColorWith(hexValue: 0x222222)
+
+let FONT_28PX = UIFont.fontWith(pixel: 28.0)
+
 
 
