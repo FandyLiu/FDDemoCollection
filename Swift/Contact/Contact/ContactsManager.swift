e1772e7101d5ac764f7f8f3e3fbdb1f294d36497
QianTuFD <liuhuan@QianTuFDdeiMac.local>
2017-05-08 18:17:03 +0800

    通讯录init

---
+++ Swift/Contact/Contact/ContactsManager.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsManager.swift
+//  Contact
+//
+//  Created by QianTuFD on 2017/5/8.
+//  Copyright © 2017年 fandy. All rights reserved.
+//
+
+import UIKit
+import Contacts
+
+class ContactsManager: NSObject {
+    func getContacts() {
+        CNContactStore
+    }
+
+}
